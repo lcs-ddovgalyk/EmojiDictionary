afe01baecefd4c0d8ebe81d9711af230db0464d1
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-11-13 21:00:50 -0500

    Created an example of an instance for emoji struct Created the emoji struct

---
+++ EmojiDictionary2/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  EmojiDictionary2
+//
+//  Created by Dovgalyk, Dima on 2019-11-13.
+//  Copyright © 2019 Dovgalyk, Dima. All rights reserved.
+//
+
+import Foundation
+//define the emoji struct
+
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var desctiprion: String
+    var usage: String
+    
+}
+// an example of creating an instance of emoji struct
+//var example = Emoji(symbol: "🍕", name: "Pizza", desctiprion: "A delicious combnation of bread, tomato sauce, and cheese.", usage: "Make when hungry")
+
+
